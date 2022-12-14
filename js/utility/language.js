af2b18807a122eb5e7fa354720cb6f2ba4f13ae6
Baeist <lars18n2@stud.kea.dk>
2022-12-14 13:25:22 +0100

    language for index page updated with en/da text

--- js/utility/language.js
+++ js/utility/language.js
@@ -42,7 +42,7 @@ function switchLanguage(){
         language = "da";
     }
 
-    // console.log(language);
+    console.log(language);
 
     renderLanguage(language);
 }
@@ -53,16 +53,60 @@ function renderLanguage(language){
 
     // Danish
     if(language === "da"){
-
-        $('#game-intro-btn').text(danish.gameintrobtn);
-
+        
+        $('#navgame').text(danish.navgame);
+        $('#buttonrules').text(danish.rulesmenulink);
+        $('#howtoplaybutton').text(danish.howtostart);
+        $('#intro-text').text(danish.introtext);
+        $('#rules-header').text(danish.rulesheader);
+        $('#rules-text').text(danish.rulestext);
+        $('#howtoheader').text(danish.howtoheader); // currently not in use
+        $('#howtotext').text(danish.howtotext);        // currently not in use
+        $('#startheader').text(danish.startheader);
+        $('#starttext').text(danish.starttext);
+        $('#creategamebutton').text(danish.creategamebutton);
+        $('#joingamebutton').text(danish.joingamebutton);
+        $('#feedbackheader').text(danish.feedbackheader);
+        $('#feedbacktext').text(danish.feedbacktext);
+        $('#fieldofstudy').text(danish.fieldofstudy);
+        $('#playername').text(danish.playername);
+        $('#modalcreategamebutton').text(danish.modalcreategamebutton);
+        $('#create-game-label').text(danish.creategamelabel);
+        $('#modalback').text(danish.modalback);
+        $('#studyfieldinput').text(danish.fieldofstudy);
+        $('#lobbyplayername').text(danish.playername);
+        $('#modaltwoback').text(danish.modalback);
+        $('#join-game-label').text(danish.joingamelabel);
+        $('#modaljoin').text(danish.modaljoin);
     }
 
     // English
     if(language === "en"){
-        
-        $('#game-intro-btn').text(english.gameintrobtn);
-
+                
+        $('#navgame').text(english.navgame);
+        $('#buttonrules').text(english.rulesmenulink);
+        $('#howtoplaybutton').text(english.howtostart);
+        $('#intro-text').text(english.introtext);
+        $('#rules-header').text(english.rulesheader);  
+        $('#rules-text').text(english.rulestext);
+        $('#howtoheader').text(english.howtoheader); // currently not in use
+        $('#howtotext').text(english.howtotext);    // currently not in use
+        $('#startheader').text(english.startheader);
+        $('#starttext').text(english.starttext);
+        $('#creategamebutton').text(english.creategamebutton);
+        $('#joingamebutton').text(english.joingamebutton);
+        $('#feedbackheader').text(english.feedbackheader);
+        $('#feedbacktext').text(english.feedbacktext);
+        $('#fieldofstudy').text(english.fieldofstudy);
+        $('#playername').text(english.playername);
+        $('#modalcreategamebutton').text(english.modalcreategamebutton);
+        $('#create-game-label').text(english.creategamelabel);
+        $('#modalback').text(english.modalback);
+        $('#studyfieldinput').text(english.fieldofstudy);
+        $('#lobbyplayername').text(english.playername);
+        $('#modaltwoback').text(english.modalback);
+        $('#join-game-label').text(english.joingamelabel);
+        $('#modaljoin').text(english.modaljoin);
     }
 
 }
