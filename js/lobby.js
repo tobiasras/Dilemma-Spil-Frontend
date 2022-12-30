ea9e366435042e2d139f57bf5c58be92aedd9e40
tobiasras <tobiasras.job@gmail.com>
2022-12-30 14:11:01 +0100

    moved index file

--- js/lobby.js
+++ js/lobby.js
@@ -20,8 +20,8 @@ class Lobby {
             // IF GAME NO WORD CHECK THAT URL MATCHES
 
             //
-            window.location.href = 'http://127.0.0.1:5500/html/game.html' + "?" + lobbyCode;
-            // window.location.href = 'http://localhost:63343/Frontend%20Dillemmaspillet/html/game.html' + "?" + lobbyCode;
+            window.location.href = 'https://gentle-meadow-0fb93b303.2.azurestaticapps.net/html/game' + "?" + lobbyCode;
+            // window.location.href = 'https://dilemmaspil.azurewebsites.net/Frontend%20Dillemmaspillet/html/game.html' + "?" + lobbyCode;
 
 
         });
@@ -49,7 +49,7 @@ class Lobby {
                 sessionStorage.setItem("player", JSON.stringify(player));
 
                 // IF GAME NO WORD CHECK THAT URL MATCHES
-                window.location.href = 'http://127.0.0.1:5500/html/game.html' + "?" + gameLobby.lobbyCode + "&join-game";
+                window.location.href = 'https://gentle-meadow-0fb93b303.2.azurestaticapps.net/html/game' + "?" + gameLobby.lobbyCode + "&join-game";
 
                 // window.location.href = 'http://localhost:63343/Frontend%20Dillemmaspillet/html/game.html' + "?" + gameLobby.lobbyCode + "&join-game";
 
--- js/utility/api.js
+++ js/utility/api.js
@@ -1,5 +1,5 @@
 async function api(end_url, method, payload) {
-    const response = await fetch("http://localhost:8080/" + end_url, {
+    const response = await fetch("https://dilemmaspil.azurewebsites.net/" + end_url, {
       headers: {
         "Content-Type": "application/json; charset=utf-8",
       },
